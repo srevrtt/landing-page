7e87dbc4776f9f7dbb50d8758454c48fd1a4b958
NLuziaf <lategreentea835@gmail.com>
2022-06-10 17:17:14 +0700

    am just gonna let that tailwind config there lmfao, i gave up. just don't touch it

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+      extend: {
+        colors: {
+          "primary" : "#34d399",
+          "background" : "#0f172a",
+        }
+      },
+    },
+    plugins: [],
+  }
